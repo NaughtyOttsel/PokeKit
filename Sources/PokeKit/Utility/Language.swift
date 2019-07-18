0ccc3ba1f567be221a9ddbb0be359245dd9628f0
James Lee <james.lee93@hotmail.co.uk>
2019-07-18 20:12:13 +0100

    Missed setting Language Properties to public

--- Sources/PokeKit/Utility/Language.swift
+++ Sources/PokeKit/Utility/Language.swift
@@ -3,15 +3,15 @@
 /// Languages within the Games and the API has been localized for
 public struct Language: LocalizableEntity, Decodable {
     /// The identifier for this resource.
-    let id: Int
+    public let id: Int
     /// The name for this resource.
-    let name: String
+    public let name: String
     /// The two-letter code of the country where this language is spoken. Note that it is not unique.
-    let iso639: String
+    public let iso639: String
     /// The two-letter code of the language. Note that it is not unique.
-    let iso3166: String
+    public let iso3166: String
     /// The name of this resource listed in different languages.
-    let names: [Name]
+    public let names: [Name]
     /// Whether or not the games are published in this language.
-    let official: Bool
+    public let official: Bool
 }
